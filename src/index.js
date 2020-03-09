0941413ba8d90c03f81be5a419e9bcc529d88405
Vlad613 <vladislav613zharnikov@gmail.com>
2020-03-09 07:11:55 +0300

    47_%

--- src/index.js
+++ src/index.js
@@ -7,22 +7,14 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 
-let rerenderApp = () => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}>
-                <App/>,
-            </Provider>
-        </BrowserRouter>,
-        document.getElementById('root'));
-}
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>,
+        </Provider>
+    </BrowserRouter>,
+    document.getElementById('root'));
 
-rerenderApp(store.getState());
-
-store.subscribe(() => {
-    let state = store.getState();
-    rerenderApp(state);
-});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/ProfileReducer.js
+++ src/redux/ProfileReducer.js
@@ -1,34 +1,39 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-let initialState ={
-        posts: [
-            {id: 0, message: 'Hi, how are you?', likeCounts: '12'},
-            {id: 1, message: "It's, my fierst post", likeCounts: '11'},
-            {id: 2, message: 'Hi!', likeCounts: '22'},
-        ],
+let initialState = {
+    posts: [
+        {id: 0, message: 'Hi, how are you?', likeCounts: '12'},
+        {id: 1, message: "It's, my fierst post", likeCounts: '11'},
+        {id: 2, message: 'Hi!', likeCounts: '22'},
+    ],
 
-        newPostText: 'it-Kamasutra'
-    };
+    newPostText: 'it-Kamasutra'
+};
 
-const profileReducer = (state=initialState, action) => {
+const profileReducer = (state = initialState, action) => {
 
     switch (action.type) {
-        case ADD_POST:
+        case ADD_POST: {
             let newPost = {
                 id: 3,
                 message: state.newPostText,
                 likeCounts: 3
             };
-            state.posts.push(newPost);
-            state.newPostText = ' ';
-            return state;
-
-        case UPDATE_NEW_POST_TEXT:
-            state.newPostText = action.newText;
-            return state;
+            let stateCopy = {...state};
+            stateCopy.posts = [...state.posts];
+            stateCopy.posts.push(newPost);
+            stateCopy.newPostText = ' ';
+            return stateCopy;
+        }
+        case UPDATE_NEW_POST_TEXT: {
+            let stateCopy={...state}
+            stateCopy.newPostText = action.newText;
+            return stateCopy;
+        }
         default:
             return state;
+
     }
 
 };
