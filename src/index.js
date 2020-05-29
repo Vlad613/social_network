49fae8a4433cfb1c200a54d3c27e402f5d7ac566
Vlad613 <55757119+Vlad613@users.noreply.github.com>
2020-05-29 15:57:03 +0300

    92%(2)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import * as serviceWorker from './serviceWorker';
-import store from "./redux/redux-store";
 import ReactDOM from "react-dom";
-import App from "./App";
 import React from "react";
-import {BrowserRouter} from "react-router-dom";
 import SamuraiJSApp from "./App";
 
 
